a7e2264afbca18bae0cfca02ce6bc7f7c46a9682
George Higbie <gehigbie@gmail.com>
2016-10-28 17:53:11 -0400

    object animation added to cow

--- app/src/main/java/com/geogehigbie/givewhitemancheese/MainActivity.java
+++ app/src/main/java/com/geogehigbie/givewhitemancheese/MainActivity.java
@@ -1,7 +1,11 @@
 package com.geogehigbie.givewhitemancheese;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        animateCow();
+    }
+
+    public void animateCow(){
+
+        ImageView cowImage = (ImageView) findViewById(R.id.cow1);
+
+        ObjectAnimator yAnim = ObjectAnimator.ofFloat(cowImage, "y",
+                cowImage.getY(), cowImage.getHeight() - 50f);
+        yAnim.setDuration(1500);
+        yAnim.setRepeatCount(20);
+        yAnim.setRepeatMode(ValueAnimator.REVERSE);
+        yAnim.setInterpolator(new AccelerateInterpolator(2f));
+        //yAnim.addUpdateListener(this);
+        //yAnim.addListener(this);
+        yAnim.start();
     }
 }
